2e6841046897e2b5f22baf951565c0296dfc6eb3
axe i-grec <tag4rce7@gmail.com>
2020-05-24 10:58:17 +0200

    Added basic user signup

--- instagram-clone-functions/functions/index.js
+++ instagram-clone-functions/functions/index.js
@@ -1,14 +1,30 @@
+const firebase = require("firebase");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 require("dotenv").config();
 
+// Initialize Express
 const app = require("express")();
 
+// Get account credientials from env and init admin SDK
 const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDA8qrE-xtEB4aWAdzOIBExE2fB3vbtUCI",
+  authDomain: "instagram-clone-7b247.firebaseapp.com",
+  databaseURL: "https://instagram-clone-7b247.firebaseio.com",
+  projectId: "instagram-clone-7b247",
+  storageBucket: "instagram-clone-7b247.appspot.com",
+  messagingSenderId: "500158409705",
+  appId: "1:500158409705:web:0442fa7928b52d99a77978",
+};
+firebase.initializeApp(firebaseConfig);
+
+// Store DB Route
 const db = admin.firestore();
 
 // Posts routes
@@ -41,7 +57,8 @@ app.post("/posts", async (req, res) => {
   }
 });
 
-app.post("/signup", (req, res) => {
+// Signup route
+app.post("/signup", async (req, res) => {
   const newUser = {
     email: req.body.email,
     password: req.body.password,
@@ -49,6 +66,17 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
   // TODO: Validate Data
+  try {
+    const data = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password);
+    res
+      .status(201)
+      .json({ message: `User ${data.user.uid} signed up sucessfully` });
+  } catch (err) {
+    res.status(500).json({ error: `Error ${err.code}` });
+    console.error(err);
+  }
 });
 
 exports.api = functions.https.onRequest(app);
